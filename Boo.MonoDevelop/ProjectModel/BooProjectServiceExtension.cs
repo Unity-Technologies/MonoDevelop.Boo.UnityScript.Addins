2b5115c5dc90e79c7ac0035c5f01ddeb859c428c
Lukasz <lukaszunity@users.noreply.github.com>
2015-05-26 10:56:20 +0200

    Throw not supported exception if UseMSBuildEngineByDefault property does not exist. Improved comment.

--- Boo.MonoDevelop/ProjectModel/BooProjectServiceExtension.cs
+++ Boo.MonoDevelop/ProjectModel/BooProjectServiceExtension.cs
@@ -9,7 +9,8 @@ namespace Boo.MonoDevelop.ProjectModel
 {
 	public class BooProjectServiceExtension : ProjectServiceExtension
 	{
-		// This is hack to force MonoDevelop to not use msbuild to build the Boo projects and instead use BooLanguageBindings.Compile
+		// This is hack to force MonoDevelop to use BooLanguageBindings.Compile to build Boo projects instead of msbuild build engine (xbuild).
+		// Starting from some 4.x release of MonoDevelop, disabling msbuild build engine can only be done in the solution/project settings.
 		protected override BuildResult Build (IProgressMonitor monitor, IBuildTarget item, ConfigurationSelector configuration)
 		{
 			var project = item as DotNetAssemblyProject;
@@ -21,6 +22,10 @@ namespace Boo.MonoDevelop.ProjectModel
 				if (msBuildProjectHandler != null) 
 				{
 					var useMSBuildEngineByDefault = msBuildProjectHandler.GetType ().GetProperty ("UseMSBuildEngineByDefault", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+					if (useMSBuildEngineByDefault == null)
+						throw new NotSupportedException ("Could not find UseMSBuildEngineByDefault property in MSBuildProjectHandler. Building of Boo projects is not supported.");
+
 					useMSBuildEngineByDefault.SetValue (msBuildProjectHandler, false);
 				}
 			}
